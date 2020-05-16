d03a097f94258109de84c2947b44838b99ff815a
Teron Sampson <sampson2448@yahoo.com>
2020-05-16 19:52:36 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class App extends React.Component{
+    constructor(){
+        super();
+        this.state={}
+    }
+    render(){
+        return(
+            <div>
+                <h1>Connected</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
